894c399a10290677505637babfead50f2a47af2c
Abhinav Adarsh <abhinavgautam08@users.noreply.github.com>
2026-04-02 12:44:16 +0530

    Provide an example SQL statement to insert a new employee record into the 'Employee_YourName' table

---
+++ 06_insert_record.sql
@@ -0,0 +1,3 @@
+-- DML Question 6: Provide an example SQL statement to insert a new employee record into the 'Employee_YourName' table.
+INSERT INTO Employee_Abhinav (Empno, Dept, Job, HireDate, Sal, FirstName, LastName, Email)
+VALUES (101, 'IT', 'Software Engineer', '2024-03-15', 75000.00, 'John', 'Doe', 'john.doe@example.com');
