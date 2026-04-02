70b4740fab468862c69953e6dfdd75b4b93d68a3
Abhinav Adarsh <abhinavgautam08@users.noreply.github.com>
2026-04-02 12:38:14 +0530

    create table in database

---
+++ 01_create_table.sql
@@ -0,0 +1,11 @@
+-- DDL Question 1: How do you create the 'Employee_YourName' table with the specified attributes and data types?
+CREATE TABLE Employee_Abhinav (
+    Empno INT PRIMARY KEY,
+    Department VARCHAR(100),
+    Job VARCHAR(100),
+    HireDate DATE,
+    Sal DECIMAL(10, 2),
+    Position VARCHAR(100),
+    FirstName VARCHAR(100),
+    LastName VARCHAR(100)
+);
