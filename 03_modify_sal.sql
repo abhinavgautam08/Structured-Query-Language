3041a6ccc7aa94c803da8e6cc821c367853a0227
Abhinav Adarsh <abhinavgautam08@users.noreply.github.com>
2026-04-02 12:41:21 +0530

    Modifying the column to explicitly allow NULL values

---
+++ 03_modify_sal.sql
@@ -0,0 +1,3 @@
+-- DDL Question 3: Can you modify the 'Sal' column in the 'Employee_YourName' table to allow NULL values?
+-- Modifying the column to explicitly allow NULL values
+ALTER TABLE Employee_Abhinav MODIFY Sal DECIMAL(10, 2) NULL;
