7b14b8c7891ed0eb65bb927fc9ef18143cd17476
Abhinav Adarsh <abhinavgautam08@users.noreply.github.com>
2026-04-02 12:43:28 +0530

    If you wanted to remove the 'Position' column from the 'Employee_YourName' table, what SQL statement would you use

---
+++ 05_drop_column.sql
@@ -0,0 +1,2 @@
+-- DDL Question 5: If you wanted to remove the 'Position' column from the 'Employee_YourName' table, what SQL statement would you use?
+ALTER TABLE Employee_Abhinav DROP COLUMN Position;
