ae15dc2e14614659af80bdd02a01c499305aa68b
Abhinav Adarsh <abhinavgautam08@users.noreply.github.com>
2026-04-02 12:39:41 +0530

    add new column named email

---
+++ 02_add_email.sql
@@ -0,0 +1,2 @@
+-- DDL Question 2: What SQL statement would you use to add a new column named 'Email' with the data type VARCHAR(255) to the 'Employee_YourName' table?
+ALTER TABLE Employee_Abhinav ADD Email VARCHAR(255);
